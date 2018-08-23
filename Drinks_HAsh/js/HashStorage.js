f2c1c7ce836868203f555a95e8127934e6d26118
Stas <chernenkosa@gmail.com>
2018-08-23 18:00:22 +0300

    create Drinks_HAsh

---
+++ Drinks_HAsh/js/HashStorage.js
@@ -0,0 +1,8 @@
+function HashStorage (drink) {
+    var self=this;
+    self.addValue = function (key,value) {}
+    self.getValue = function (key) {}
+    self.deleteValue = function (key) {}
+}
+
+var drinkStorage = new HashStorage();
